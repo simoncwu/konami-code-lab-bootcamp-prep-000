4883e7d570ee55c135b59d51141e1ce201e1da45
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-12 22:28:54 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ function init() {
     const key = parseInt(e.detail || e.which);
     
     if (key == code[index]) {
-      index ++;
+      index++;
       
       if (index >= alphabet.length) {
         alert();
